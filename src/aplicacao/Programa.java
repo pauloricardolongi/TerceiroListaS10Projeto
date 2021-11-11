362d33a29880771382d4d2c2416be3257f79e164
pauloricardolongi <pauloricardolongi@gmail.com>
2021-11-11 03:33:03 -0300

    Ultima versão Projeto Lista

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -23,6 +23,11 @@ public class Programa {
 			System.out.println("Funcionário #" + (i + 1) + ":");
 			System.out.print("Id: ");
 			Integer id = sc.nextInt();
+			while(hasId(list, id)) {
+				System.out.println("Este Id já existe! Tente novamente!");
+				id = sc.nextInt();
+			}
+			
 			System.out.print("Nome: ");
 			sc.nextLine();
 			String nome = sc.nextLine();
@@ -36,26 +41,29 @@ public class Programa {
 		System.out.println();
 		System.out.print("Entre com Id do salário do funcionário que receberá aumento: ");
 		int idSalario = sc.nextInt();
-		Integer pos = posicao(list, idSalario);
-		if (pos == null) {
+		
+		Funcionario func = list.stream().filter(x -> x.getId()== idSalario).findFirst().orElse(null);
+		
+		//Integer pos = posicao(list, idSalario);
+		if (func == null) {
 			System.out.println("Este id não existe!");
 		}
 		else {
 			System.out.print("Entre com o valor da porcentagem!");
 			double porcento = sc.nextDouble();
-			list.get(pos).aumentoSalario(porcento);
+			func.aumentoSalario(porcento);
 			
 		}
 		System.out.println();
 		System.out.println("Lista de Funcionários: ");
-		for (Funcionario func : list) {
-			System.out.println(func);
+		for (Funcionario e : list) {
+			System.out.println(e);
 			
 		}
 		
 		sc.close();
 
-	}//funcao auxiliar: para procurar a posição do  elemento na lista
+	}
 	public static Integer posicao (List<Funcionario> list, int id) {
 		for (int i=0; i < list.size(); i++) {
 			if(list.get(i).getId()== id) {
@@ -63,6 +71,10 @@ public class Programa {
 			}
 		}
 		return null;
+	}//funcao auxiliar: para ver se id existe
+	public static boolean hasId(List <Funcionario>list, int id) {
+		Funcionario func = list.stream().filter(x -> x.getId()== id).findFirst().orElse(null);
+		return func != null;
 	}
 
 }
